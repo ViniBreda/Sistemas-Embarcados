93b5fafc063d8246129bfbde99414a13e872d899
Vinicius <v_i_breda@hotmail.com>
2019-10-27 17:04:06 -0300

    commenting main.cpp

--- 4_Arquivos/3_ProjetoSOE/ProjetoSOE/main.cpp
+++ 4_Arquivos/3_ProjetoSOE/ProjetoSOE/main.cpp
@@ -60,7 +60,7 @@ int main(int argc, char** argv) {
                 std::string plateR(plate, 8, 1);
                 plateR[1] = '\0';
                 //std::cout << plateR << std::endl;
-                if (plateR != "1"){ 
+                if (plateR != "1"){ // se houverem duas possibilidades de placa, salva a segunda em outra variável
                     std::string plateL2(plate, 77, 3);
                     plateL2[3] = '\0';
                     std::string plateN2(plate, 80, 4);
@@ -69,10 +69,10 @@ int main(int argc, char** argv) {
                     std::cout << plateL2 << ' ' << plateN2 << std::endl;
                 }
             }
-            else
+            else // se não reconhece alguma placa, mostra somente o output do comando de terminal
                 std::cout << plate << std::endl;
         }
-        if(waitKey(30) >= 0) break;
+        if(waitKey(30) >= 0) break; // fecha o programa se 
                 
     }
     system("clear");
